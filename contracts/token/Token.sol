8521390a0f66afb5e0afc7d9f203cc29037253fd
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-11 19:26:28 +0400

    :sparkles: Token contract for tests

---
+++ contracts/token/Token.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 totalSupply_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, totalSupply_);
+    }
+}
