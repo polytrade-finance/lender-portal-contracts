6276ba3a96c9b86f9435739cb9984eb91a9d78ed
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-16 17:13:59 +0400

    :zap: improve RewardSystem with uniswap price and bonusAPY per user

--- contracts/RewardSystem.sol
+++ contracts/RewardSystem.sol
@@ -6,17 +6,26 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract RewardSystem {
+    address USDT = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
+    address WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    address TRADE = 0x692AC1e363ae34b6B489148152b12e2785a3d8d6;
+    address router = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff;
 
-    IERC20Metadata USDT = IERC20Metadata(0xc2132D05D31c914a87C6611C10748AEb04B58e8F);
-    IERC20Metadata WMATIC = IERC20Metadata(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
-    IERC20Metadata tradeToken = IERC20Metadata(0x692AC1e363ae34b6B489148152b12e2785a3d8d6);
-    IUniswapV2Router router = IUniswapV2Router(0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff);
+    mapping(address => uint16) private _userBonusAPY;
 
-    function getAmountOfTrade(uint amount) public view returns(uint) {
+    function getAmountOfTrade(uint256 amount) external view returns (uint256) {
         address[] memory path = new address[](3);
-        path[0] = address(USDT);
-        path[1] = address(WMATIC);
-        path[2] = address(tradeToken);
-        return router.getAmountsOut(amount, path)[2];
+        path[0] = USDT;
+        path[1] = WMATIC;
+        path[2] = TRADE;
+        return IUniswapV2Router(router).getAmountsOut(amount, path)[2];
+    }
+
+    function setUserBonusAPY(address lender, uint16 bonusAPY) external {
+        _userBonusAPY[lender] = bonusAPY;
+    }
+
+    function getUserBonusAPY(address lender) external view returns (uint16) {
+        return _userBonusAPY[lender];
     }
 }
