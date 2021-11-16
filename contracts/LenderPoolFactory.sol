f0e9abe11ea4987965f23fd56f18a224819bf8b2
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-16 17:13:11 +0400

    :rotating_light: add EIP 1167 clone factory

---
+++ contracts/LenderPoolFactory.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.8.10;
+
+import "./LenderPool.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "hardhat/console.sol";
+
+contract LenderPoolFactory {
+    using Clones for address;
+    address lenderInstance;
+
+    constructor(address lenderInstance_) {
+        lenderInstance = lenderInstance_;
+    }
+
+    function clonePool() external returns (address) {
+        address clonedPool = lenderInstance.clone();
+        console.log("%s <<<", clonedPool);
+        return clonedPool;
+    }
+
+    function getDeterministicPoolAddress(bytes32 salt)
+        external
+        view
+        returns (address)
+    {
+        address clonedPool = lenderInstance.predictDeterministicAddress(
+            salt,
+            msg.sender
+        );
+        return clonedPool;
+    }
+
+    function cloneDeterministicPool(bytes32 salt) external returns (address) {
+        address clonedPool = lenderInstance.predictDeterministicAddress(
+            salt,
+            msg.sender
+        );
+        console.log("%s <<<", clonedPool);
+        return clonedPool;
+    }
+}
