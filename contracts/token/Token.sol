1a052667e23f8417b6993598a281de4aa7fddba3
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-17 13:07:05 +0400

    :art: add decimals on sc storage

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+
+    uint8 private _decimals;
+
     constructor(
         string memory name_,
         string memory symbol_,
         uint256 totalSupply_
     ) ERC20(name_, symbol_) {
         _mint(msg.sender, totalSupply_);
+        _decimals = decimals_;
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return 6;
+        return _decimals;
     }
 }
