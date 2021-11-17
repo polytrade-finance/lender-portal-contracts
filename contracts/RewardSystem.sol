66039511b23ebabd2e41d6ba95ebffddaaea3ed9
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-17 13:13:34 +0400

    :art: add SPDX

--- contracts/RewardSystem.sol
+++ contracts/RewardSystem.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
 import "hardhat/console.sol";
--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -1,9 +1,9 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-
     uint8 private _decimals;
 
     constructor(
