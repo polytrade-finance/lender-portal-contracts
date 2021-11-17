0a932e9910395eb4dba8a98cdf34b4691030d043
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-17 13:05:55 +0400

    :sparkles: add DAI to RewardSystem

--- contracts/RewardSystem.sol
+++ contracts/RewardSystem.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract RewardSystem {
+    address DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
     address USDT = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
     address WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address TRADE = 0x692AC1e363ae34b6B489148152b12e2785a3d8d6;
@@ -16,6 +17,14 @@ contract RewardSystem {
     function getAmountOfTrade(uint256 amount) external view returns (uint256) {
         address[] memory path = new address[](3);
         path[0] = USDT;
+        path[1] = WMATIC;
+        path[2] = TRADE;
+        return IUniswapV2Router(router).getAmountsOut(amount, path)[2];
+    }
+
+    function getAmountTradeForDAI(uint amount) external view returns (uint) {
+        address[] memory path = new address[](3);
+        path[0] = DAI;
         path[1] = WMATIC;
         path[2] = TRADE;
         return IUniswapV2Router(router).getAmountsOut(amount, path)[2];
