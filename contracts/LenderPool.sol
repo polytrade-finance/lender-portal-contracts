27e593fcfda725273c0dea38a660c2340038e3b4
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-19 03:05:03 +0400

    :art: add interfaces

--- contracts/LenderPool.sol
+++ contracts/LenderPool.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "hardhat/console.sol";
+import "./interfaces/ILenderPool.sol";
+import "./interfaces/IUniswapV2Router.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
