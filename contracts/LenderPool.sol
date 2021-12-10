13236fc8a3031a5a6ec5311b7375ff93f07ac5a9
Aboudjem <boudjemaa.adam@gmail.com>
2021-12-10 16:19:39 +0400

    :rotating_light: lint fix

--- contracts/LenderPool.sol
+++ contracts/LenderPool.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-
 /// @author Polytrade
 /// @title LenderPool V1
 contract LenderPool is ILenderPool, Ownable, Pausable {
