fb6a6565a17469566d7f3cebfd85d8911a3b35d0
Aboudjem <boudjemaa.adam@gmail.com>
2021-12-23 17:37:52 +0400

    :truck: rename function getNumberOfRounds > getLatestRound

--- contracts/LenderPool.sol
+++ contracts/LenderPool.sol
@@ -148,12 +148,12 @@ contract LenderPool is ILenderPool, Ownable {
     }
 
     /**
-     * @notice Returns the number of rounds for the a specific lender
+     * @notice Returns the latest round for a specific lender
      * @param lender, address of the lender to be checked
-     * @return returns the number of rounds for a specific Lender
+     * @return returns the latest round for a specific Lender
      */
-    function getNumberOfRounds(address lender) external view returns (uint) {
-        return _lenderInfo[lender].roundCount;
+    function getLatestRound(address lender) external view returns (uint) {
+        return _lenderInfo[lender].roundCount - 1;
     }
 
     /**
